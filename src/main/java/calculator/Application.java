dc483f8583acc78a9a7ebd0f5d9100c50a348f59
JinGoo <zzgg3040@naver.com>
2025-10-19 23:02:30 +0900

    feat(calculator): implement summing numbers separated by comma or colon

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
+            StringTokenizer st = new StringTokenizer(input, ",|:");
+            long answer = 0;
+            while (st.hasMoreTokens()) {
+                int cur = Integer.parseInt(st.nextToken());
+                answer += cur;
+            }
+            System.out.println("결과 : " + answer);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
